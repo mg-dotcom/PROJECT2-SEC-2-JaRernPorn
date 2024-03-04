aa605e5761a1fdeabb51cff0d9ad3da48c36f93d
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-03-05 00:15:40 +0700

    asd

--- src/App.vue
+++ src/App.vue
@@ -4,44 +4,56 @@ import { reactive } from "vue";
 const page = reactive({
   homePage: true,
 });
+
+const playButton = () => {
+  // Add your logic here
+};
+
+const howToPlayButton = () => {
+  // Add your logic here
+};
 </script>
 
 <template>
   <section class="้HomePage" v-show="page.homePage">
-    <div class="h-screen w-full bg-main-bgColor p-5">
-      <div class="border-double border-8 border-title box-border h-full w-full">
+    <div style="background-image: url('./assets/homePage/test.png')">
+      <div class="h-screen w-full bg-main-bgColor p-5">
         <div
-          class="logo-title text-center text-title pt-title sm:text-title-size mobile:text-mobile-title-size"
+          class="border-double border-8 border-title box-border h-full w-full"
         >
-          <div class="font-alkatra font-medium leading-title">HELLO !</div>
-          <div class="font-alkatra font-medium leading-title">CHINESE</div>
-        </div>
-        <div class="text-black text-center">
-          <p
-            class="font-outfit font-medium tracking-sub-title sm:text-sub-title-size"
+          <div
+            class="logo-title text-center text-title pt-title sm:text-title-size mobile:text-mobile-title-size"
           >
-            LET'S PRACTICE YOUR CHINESE VOCABULARY
-          </p>
-        </div>
-
-        <div id="home-page-button">
-          <!-- Play Button -->
-          <div id="play-button" class="flex justify-center pt-12">
-            <img
-              class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
-              src="./assets/homePage/play-button.svg"
-              alt="play-button"
-              @click="playButton"
-            />
+            <div class="font-alkatra font-medium leading-title">HELLO !</div>
+            <div class="font-alkatra font-medium leading-title">CHINESE</div>
           </div>
-          <!-- Flashcard Button -->
-          <div id="HTP-button" class="flex justify-center pt-6">
-            <img
-              class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
-              src="./assets/homePage/flashcard-button.svg"
-              alt="HTP-button"
-              @click="howToPlayButton"
-            />
+          <div class="text-black text-center">
+            <p
+              class="font-outfit font-medium tracking-sub-title sm:text-sub-title-size"
+            >
+              LET'S PRACTICE YOUR CHINESE VOCABULARY
+            </p>
+          </div>
+
+          <div id="home-page-button">
+            <!-- Play Button -->
+            <div id="play-button" class="flex justify-center pt-12">
+              <img
+                class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
+                src="./assets/homePage/play-button.svg"
+                alt="play-button"
+                @click="playButton"
+              />
+            </div>
+            <!-- Flashcard Button -->
+            <div id="HTP-button" class="flex justify-center pt-6">
+              <img
+                class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
+                src="./assets/homePage/flashcard-button.svg"
+                alt="HTP-button"
+                @click="howToPlayButton"
+              />
+            </div>
           </div>
         </div>
       </div>
