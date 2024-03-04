302a2ccfe955121a1f77f5db87f5f2a61a6b4545
h88ych <hathaitat88@gmail.com>
2024-03-04 23:38:01 +0700

    First try (please don't look)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <script setup>
+import { ref } from 'vue'
+// import folder from './components/folder.vue'
+const flashCard = ref([])
 
+const createNewFolder = () => {
+  const newFolder = { name: nameFolder }
+  flashCard.value.push(newFolder)
+}
 </script>
- 
-<template>
-<div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</div>
-</template>
- 
-<style scoped>
 
-</style>
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">FLASHCARD</h1>
+    <button class="border-2 border-black mt-10" @click="createNewFolder">
+      Add New Folder
+    </button>
+  </div>
+
+  <div>
+    <!-- <img src="/Folder.svg" class="w-40 mt-10" /> -->
+    <div
+      class="bg-gray-200 border border-gray-300 rounded-md shadow-md w-60 h-52 mt-10 ml-10"
+    >
+      <button class="text-2xl">...</button>
+      <div v-for="(fish, index) in flashCard">
+        <h1 class="flex justify-center mt-16 text-3xl">{{ fish.name }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/folder.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div>
+    <!-- <img src="/Folder.svg" class="w-40 mt-10" /> -->
+    <div
+      class="bg-gray-200 border border-gray-300 rounded-md shadow-md w-60 h-52 mt-10 ml-10"
+    >
+      <button class="text-2xl">...</button>
+      <h1 class="flex justify-center mt-16 text-3xl">Theeee</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
