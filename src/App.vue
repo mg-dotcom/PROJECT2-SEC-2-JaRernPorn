11ef1aa80fdb68c896577a1928d9a3eacb7ca158
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-03-06 23:27:06 +0700

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { reactive, ref, computed } from "vue"; // Add the import statement for reactive
 import close from "./components/icons/iconClose.vue";
+import OptionCollection from "./components/OptionCollection.vue";
+import DeleteCollection from "./components/DeleteCollection.vue";
+import EditCollection from "./components/EditCollection.vue";
+
 const page = reactive({
   flashcard: true,
 });
 
 const popup = reactive({
   newCollection: false,
+  OptionCollection: false,
 });
 
 const addNewCollectionName = () => {
@@ -17,6 +22,17 @@ const closeButton = () => {
   popup.newCollection = false;
 };
 
+const showOption = (item) => {
+  popup.OptionCollection = !popup.OptionCollection;
+  SelectedIndex.value = item;
+  console.log(item);
+};
+
+const closeOption = () => {
+  popup.OptionCollection = false;
+};
+
+const SelectedIndex = ref(null);
 const collections = ref([]);
 const newCollectionName = ref("");
 const localCollections = JSON.parse(localStorage.getItem("collections")) || [];
@@ -42,14 +58,18 @@ const computedCollections = computed(() => {
   return collections.value;
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const element = document.getElementById("truncateText");
-  const maxCharacters = 27; // Adjust the maximum number of characters as needed
+// Add the deleteCollection method
+const deleteCollection = (collectionId) => {
+  const updatedCollections = collections.value.filter(
+    (item, index) => index !== collectionId
+  );
 
-  if (element.textContent.length > maxCharacters) {
-    element.classList.add("custom-truncate");
-  }
-});
+  localStorage.setItem("collections", JSON.stringify(updatedCollections));
+
+  collections.value = updatedCollections;
+};
+
+// Add the editCollection method
 </script>
 <template>
   <section class="flashcard-home" v-show="page.flashcard">
@@ -58,7 +78,8 @@ document.addEventListener("DOMContentLoaded", function () {
       :class="{ absolute: popup.newCollection }"
     >
       <div
-        class="bg-[#fff7e6] rounded-lg border w-[500px] h-[550px] p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] text-center flex flex-col overflow-auto"
+        class="z-0 bg-[#fff7e6] rounded-lg border w-[500px] h-[550px] p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] text-center flex flex-col overflow-auto"
+        @click.self="closeOption"
       >
         <div class="text-3xl font-mono font-bold text-[#4096ff]">
           Flash Card
@@ -66,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <div class="text-start text-sm font-semibold mt-3 mb-2">
           <div>
             <h1
-              class="cursor-pointer inline hover:bg-[#f4ead8] p-[4px] rounded-xl transition-all duration-200"
+              class="cursor-pointer inline hover:bg-[#f4ead8] p-[4px] rounded-xl transition-all duration-[270ms]"
               @click="addNewCollectionName"
             >
               Add new +
@@ -80,31 +101,68 @@ document.addEventListener("DOMContentLoaded", function () {
         >
           No collection added yet
         </div>
-        <div v-else class="grid grid-cols-3 gap-2 p-2 text-center">
-          <div v-for="(item, index) in computedCollections" :key="item">
-            <div class="relative">
-              <img class="w-[130px]" src="./assets/collection.svg" alt="" />
-              <img
-                class=" z-10 opacity-70 scale-[55%] hover:bg-gray-400 rounded-full w-10 h-10 p-2 cursor-pointer absolute top-2 right-[7.5px]"
-                src="./assets/option.svg"
-                alt="option"
-              />
 
-              <div
-                class="absolute w-[130px] inset-0 flex items-center justify-center overflow-hidden p-2"
-              >
+        <div class="relative">
+          <div
+            v-if="localCollections !== null"
+            class="grid grid-cols-3 gap-2 p-2 text-center"
+            @click.self="closeOption"
+          >
+            <div v-for="(item, index) in computedCollections" :key="item">
+              <div class="relative flex flex-col items-center">
+                <img class="w-[130px]r" src="./assets/collection.svg" alt="" />
+
+                <img
+                  class="z-10 opacity-70 scale-[55%] hover:bg-gray-400 rounded-full w-10 h-10 p-2 cursor-pointer absolute top-2 right-[1.2px] transition-all duration-[270ms]"
+                  src="./assets/option.svg"
+                  alt="option"
+                  @click="showOption(index)"
+                />
+
                 <div
-                  class="text-xl font-semibold whitespace-normal break-all overflow-ellipsis max-h-[27px]"
+                  class="absolute w-[145px] inset-0 flex items-center justify-center overflow-hidden p-2 cursor-pointer"
                 >
-                  {{ item }}
+                  <div
+                    class="text-xl font-semibold whitespace-normal break-all overflow-ellipsis max-h-[27px]"
+                  >
+                    {{ item }}
+                  </div>
                 </div>
+                <OptionCollection
+                  v-show="popup.OptionCollection && SelectedIndex === index"
+                  :collectionId="index"
+                  class="absolute left-[140px] z-40"
+                >
+                  <template #delete>
+                    <DeleteCollection
+                      @click="deleteCollection(index)"
+                      class="flex gap-3 hover:bg-gray-100 transition duration-[270ms] ease-in-out p-1 rounded-lg"
+                    >
+                      <iconDelete>
+                        <template #content>
+                          <div>Delete this collection</div>
+                        </template>
+                      </iconDelete>
+                    </DeleteCollection>
+                  </template>
+                  <template #edit>
+                    <EditCollection
+                      class="flex gap-3 hover:bg-gray-100 transition duration-[270ms] ease-in-out p-1 rounded-lg"
+                    >
+                      <iconEdit>
+                        <template #content> Edit this collection </template>
+                      </iconEdit>
+                    </EditCollection>
+                  </template>
+                </OptionCollection>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <section
-        class="popup-newCollection absolute z-20"
+        class="popup-newCollection absolute"
         v-show="popup.newCollection"
       >
         <div
@@ -135,13 +193,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 <input
                   type="text"
                   v-model="newCollectionName"
-                  class="border-2 border-[#4096ff] rounded-md p-2 w-[400px] focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-200"
+                  class="border-2 border-[#4096ff] rounded-md p-2 w-[400px] focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
                   placeholder="Collection name"
                   @keydown.enter="addNewCollection"
                 />
                 <div>
                   <button
-                    class="bg-[#4096ff] text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-200"
+                    class="bg-[#4096ff] text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
                     @click="addNewCollection"
                   >
                     ADD
---
+++ src/components/OptionCollection.vue
@@ -0,0 +1,16 @@
+<script setup>
+import iconDelete from "./icons/iconDelete.vue";
+import iconEdit from "./icons/iconEdit.vue";
+import { defineProps, defineEmits } from "vue";
+
+</script>
+
+<template>
+  <div
+    class="max-w-[270px] p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-full"
+  >
+  <slot name="delete"></slot>
+  <hr class="my-2 border-gray-200 dark:border-gray-700" />
+  <slot name="edit"></slot>
+  </div>
+</template>
