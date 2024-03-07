772624dc6373e3cede77ce13b558ae009f5ac365
h88ych <hathaitat88@gmail.com>
2024-03-08 01:08:34 +0700

    Create Flashcard

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,13 @@ const formInput = ref(false)
 const nameFolder = ref('')
 
 const createNewFolder = () => {
-  const newFolder = { name: nameFolder }
+  const newFolder = { name: nameFolder.value }
   flashCard.value.push(newFolder)
   formInput.value = false
+  console.log(nameFolder.value)
+  console.log(flashCard.value)
+
+  nameFolder.value = ''
 }
 
 const openForm = () => {
@@ -26,12 +30,13 @@ const openForm = () => {
 
   <div>
     <!-- <img src="/Folder.svg" class="w-40 mt-10" /> -->
-    <div
-      class="bg-gray-200 border border-gray-300 rounded-md shadow-md w-60 h-52 mt-10 ml-10"
-    >
-      <button class="text-2xl">...</button>
-      <div v-for="(fish, index) in flashCard">
-        <h1 class="flex justify-center mt-16 text-3xl">{{ fish.name }}</h1>
+
+    <div v-for="(folder, index) in flashCard">
+      <div
+        class="bg-gray-200 border border-gray-300 rounded-md shadow-md w-60 h-52 mt-10 ml-10"
+      >
+        <button class="text-2xl">...</button>
+        <h1 class="flex justify-center mt-16 text-3xl">{{ folder.name }}</h1>
       </div>
     </div>
   </div>
