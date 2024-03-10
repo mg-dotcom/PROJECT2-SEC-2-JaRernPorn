0cdf27d68fe29bd6ecf8c0983a7eb92338a5ae60
ONTTHDO <111364693+ONTTHDO@users.noreply.github.com>
2024-03-10 15:20:55 +0700

    test add cards in flash card page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Game2 from './components/Game2.vue';
+import FlashCard from './components/FlashCard.vue'
 
 </script>
  
 <template>
 <div>
-  <Game2></Game2>
+  <!-- <Game2></Game2> -->
+  <FlashCard></FlashCard>
 </div>
 </template>
  
