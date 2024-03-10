06c2827bda49058a509dbdc4b1d2a604d558cb30
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-03-10 16:30:40 +0700

    data added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
-<script setup></script>
+<script setup>
+import categories from '../data/categories.json'
+</script>
 
 <template>
   <div class="w-full h-screen bg-main-bgColor">
-    <div class="flex flex-col gap-5 p-6 py-8">
+    <div class="flex flex-col gap-5 px-16 py-12">
       <div class="flex justify-between items-center">
         <h1 class="font-outfit text-title text-3xl font-semibold sm:text-4xl">
           Category: Fruits
@@ -50,9 +52,9 @@
       </div>
     </div>
 
-    <div class="flex justify-end px-6 lg:px-36">
+    <div class="flex justify-end px-6 lg:px-32">
       <button
-        class="bg-title rounded-2xl text-white font-outfit font-semibold w-20 h-8 sm:text-2xl sm:w-28 sm:h-14 sm:rounded-3xl hover:bg-button-bgColor lg:w-40"
+        class="bg-title rounded-2xl text-white font-outfit font-semibold w-20 h-8 sm:text-2xl sm:w-28 sm:h-14 sm:rounded-3xl hover:bg-button-bgColor lg:w-36 lg:h-12"
       >
         Check
       </button>
