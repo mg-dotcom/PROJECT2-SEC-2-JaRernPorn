d366936fbb68f5dfb387d5e4ffba4b2a7f71ba6c
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-03-11 22:14:40 +0700

    update add flashcard ui

--- src/components/AddCollection.vue
+++ src/components/AddCollection.vue
@@ -1,54 +1,60 @@
-<script setup>
-import { ref, reactive, computed } from 'vue'
-</script>
+<script setup></script>
 
 <template>
-  <div class="w-full h-screen bg-main-bgColor">
-    <!-- Add Collection -->
-    <div class="flex justify-between p-6 items-center">
-      <img
-        src="/flashcard/setting vector.svg"
-        alt="setting"
-        class="w-8 cursor-pointer lg:w-10"
-      />
-      <img
-        src="/flashcard/close.svg"
-        alt="close"
-        class="w-3 cursor-pointer lg:w-4"
-      />
-    </div>
-
-    <div class="flex justify-center">
-      <h1 class="text-5xl text-title font-outfit font-semibold">
-        Collection Name
-      </h1>
-    </div>
-
-    <div class="flex flex-col gap-y-8">
-      <div class="flex flex-col">
-        <label for="chineseword" class="font-outfit text-xl"
-          >Chinese word</label
+  <div class="w-full h-screen grid place-content-center">
+    <div class="bg-white shadow-xl rounded-2xl px-4 py-6 w-96 h-90">
+      <div class="flex justify-center py-2">
+        <span class="font-outfit text-3xl font-semibold text-title"
+          >Add new flashcard</span
         >
-        <input type="text" class="rounded-xl shadow-md w-1/4 h-11" />
       </div>
 
-      <div class="flex flex-col">
-        <label for="chineseword" class="font-outfit text-xl">Pinyin</label>
-        <input type="text" class="rounded-xl shadow-md w-1/4 h-11" />
+      <div class="grid place-content-center py-2">
+        <div class="flex flex-col py-2 gap-y-1">
+          <label for="chineseword" class="text-black font-outfit"
+            >Chinese Word</label
+          >
+          <input
+            type="text"
+            id="chineseword"
+            class="border border-black bg-white rounded-md p-1 w-60"
+            placeholder="苹果"
+          />
+        </div>
+
+        <div class="flex flex-col py-2 gap-y-1">
+          <label for="pinyin" class="text-black font-outfit">Pinyin</label>
+          <input
+            type="text"
+            id="pinyin"
+            class="border border-black bg-white rounded-md p-1 w-60"
+            placeholder="píngguǒ"
+          />
+        </div>
+
+        <div class="flex flex-col py-2 gap-y-1">
+          <label for="meaning" class="text-black font-outfit">Meaning</label>
+          <input
+            type="text"
+            id="meaning"
+            class="border border-black bg-white rounded-md p-1 w-60"
+            placeholder="Apple"
+          />
+        </div>
       </div>
 
-      <div class="flex flex-col">
-        <label for="chineseword" class="font-outfit text-xl">Meaning</label>
-        <input type="text" class="rounded-xl shadow-md w-1/4 h-11" />
+      <div class="flex justify-between px-12 py-2">
+        <button
+          class="font-outfit font-semibold text-white bg-title rounded-xl w-24 p-2 hover:bg-button-bgColor"
+        >
+          CANCEL
+        </button>
+        <button
+          class="font-outfit font-semibold text-white bg-title rounded-xl w-24 p-2 hover:bg-button-bgColor"
+        >
+          ADD
+        </button>
       </div>
-    </div>
-
-    <div class="flex justify-center py-8">
-      <button
-        class="bg-title text-white font-outfit font-medium text-xl px-4 py-2 tracking-wide rounded-xl hover:bg-button-bgColor"
-      >
-        ADD
-      </button>
     </div>
   </div>
 </template>
