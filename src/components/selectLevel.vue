90f4da3db37e471710e834c700c433dd6037ca46
h88ych <hathaitat88@gmail.com>
2024-03-13 23:09:54 +0700

    Finish show nameCate in unit

--- src/components/selectLevel.vue
+++ src/components/selectLevel.vue
@@ -1,32 +1,62 @@
 <script setup>
-import { ref, reactive } from 'vue'
-import categories from '../../data/categories.json'
+import { ref, reactive, computed } from 'vue'
+import { categories } from '../../data/categories.json'
 
-const currentIndexItem = ref(0)
 const currentIndexCate = ref(0)
+const currentIndexUnit = ref(0)
 
-const init = () => {
-  currentIndexItem.value = 0
-  currentIndexCate.value = 0
+const categoryPage = ref(true)
+const unitPage = ref(false)
+
+// const init = () => {
+//   currentIndexItem.value = 0
+//   currentIndexCate.value = 0
+// }
+
+// const allPage = reactive({
+//   homePage: true,
+//   categoryPage: false,
+//   unitPage: false
+// })
+
+// const backToHome = () => {
+//   allPage.homePage = true
+//   allPage.categoryPage = false
+//   allPage.wordListPage = false
+//   init()
+// }
+
+const showUnit = (cateIndex) => {
+  currentIndexCate.value = cateIndex //เก็บ index category
+  currentIndexUnit.value = cateIndex
+
+  console.log(cateIndex)
+  console.log(categories[currentIndexCate.value].name)
+  console.log(categories[currentIndexCate.value].units)
+  console.log(categories[currentIndexCate.value].units[0])
+
+  unitPage.value = true
+  categoryPage.value = false
 }
 
-const allPage = reactive({
-  homePage: true,
-  categoryPage: false,
-  playgamePage: false,
-  wordListPage: false
+const backToCategory = () => {
+  unitPage.value = false
+  categoryPage.value = true
+}
+
+const currentCategory = computed(() => {
+  return categories[currentIndexCate.value].name
 })
 
-const backToHome = () => {
-  allPage.homePage = true
-  allPage.categoryPage = false
-  allPage.wordListPage = false
-  init()
-}
+const currentItem = computed(() => {
+  const currentUnit = categories[currentIndexCate.value].units
+  const firstItem = currentUnit.map((unit) => unit.items[0]) //map อันแรกในแต่ละunit
+  return firstItem
+})
 </script>
 
 <template>
-  <section class="category">
+  <section class="category" v-if="categoryPage">
     <div class="p-7 bg-main-bgColor min-h-screen">
       <header>
         <!-- Back to home Button -->
@@ -65,6 +95,7 @@ const backToHome = () => {
                   :src="category.image"
                   :alt="category.name"
                   class="hover:drop-shadow-lg"
+                  @click="showUnit(cateIndex)"
                 />
               </div>
 
@@ -76,42 +107,34 @@ const backToHome = () => {
     </div>
   </section>
 
-  <!-- <section class="unit" v-if="false">
+  <section class="unit" v-if="unitPage">
     <div class="bg-main-bgColor h-screen w-full">
       <div class="flex justify-between">
-        <img src="/Arrow-button.svg" alt="Arrow" class="pt-20 pl-20" />
-        <div v-for="(category, categoriesIndex) in categories">
-          <h1
-            class="text-3xl text-wrongPopup-size font-semibold font-outfit text-title pt-28 pr-20"
-          >
-            {{ category[0].name }}
-          </h1>
-        </div>
+        <img
+          src="/Arrow-button.svg"
+          alt="Arrow"
+          class="pt-20 pl-20 cursor-pointer"
+          @click="backToCategory"
+        />
+        <h1
+          class="text-3xl text-wrongPopup-size font-semibold font-outfit text-title pt-28 pr-20"
+        >
+          {{ currentCategory }}
+        </h1>
         <img src="/Setting.svg" alt="Setting" class="pt-20 pr-20" />
       </div>
 
-      <div v-for="(category, categoriesIndex) in categories">
-        <div class="flex justify-center pt-20">
-          <div class="flex flex-wrap justify-center gap-44 w-[800px] h-[500px]">
-            <div>
-              <div class="w-44 h-44 bg-[#F9D986] rounded-[50px]"></div>
-              <img src="/3stars.svg" alt="" class="mt-4 -ml-1" />
-            </div>
-
-            <div>
-              <div class="w-44 h-44 bg-[#F9D986] rounded-[50px]"></div>
-              <img src="/2stars.svg" alt="" class="mt-4 -ml-1" />
-            </div>
-
-            <div>
-              <div class="w-44 h-44 bg-[#D9D9D9] rounded-[50px] -mt-36"></div>
-              <img src="/0star.svg" alt="" class="mt-4 -ml-1" />
+      <div class="flex justify-center pt-20">
+        <div class="flex flex-wrap justify-center gap-44 w-[800px] h-[500px]">
+          <div v-for="item in currentItem" :key="item.id">
+            <div class="w-44 h-44 bg-[#F9D986] rounded-[50px]">
+              <img :src="item.src" :alt="item.meaning" />
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section> -->
+  </section>
 </template>
 
 <style></style>
