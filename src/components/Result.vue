71c0a7ec67c61b51aa7fa1cf85eb174c6be01033
h88ych <hathaitat88@gmail.com>
2024-03-19 11:10:16 +0700

    fix ui

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { defineProps } from 'vue'
 import category from '../../data/categories.json'
-
+import SoundButton from './SoundButton.vue'
 
 const props = defineProps({
-  categoryIndex: { //เอาไว้ดึงข้อมูลของหมวดหมู่มาใช้
+  categoryIndex: {
+    //เอาไว้ดึงข้อมูลของหมวดหมู่มาใช้
     type: Number,
     default: 0 //ให้เป็นที่ 0 ไว้ก่อน
   },
-  unitIndex: { //รับมาเพื่อที่จะเอาไว้ดึงข้อมูลของ unit อันนั้นๆมาใช้
+  unitIndex: {
+    //รับมาเพื่อที่จะเอาไว้ดึงข้อมูลของ unit อันนั้นๆมาใช้
     type: Number,
     default: 0 //ให้เป็นที่ 0 ไว้ก่อน
   }
@@ -18,77 +20,105 @@ const props = defineProps({
 
 const currentCategory = category.categories[props.categoryIndex]
 const items = currentCategory.units[props.unitIndex].items
-
 </script>
 
 <template>
-
   <div class="bg-main-bgColor h-screen w-full p-5">
-
     <div class="border-double border-8 border-title box-border h-full w-full">
-
       <div class="flex justify-between pt-10 px-20">
         <div class="setting flex">
-          <img src="/setting.svg" alt="setting button" class="w-10" />
-
+          <img
+            src="/setting.svg"
+            alt="setting button"
+            class="w-10 cursor-pointer"
+          />
         </div>
-        <div class=" font-semibold font-outfit text-title ">
-          <h1>HOME</h1>
+        <div class="font-semibold font-outfit text-title">
+          <img
+            src="../../public/homeButton.svg"
+            alt="home button"
+            class="cursor-pointer size-14"
+          />
         </div>
-
       </div>
 
       <div class="flex justify-center mt-3">
-        <h1 class="text-title text-5xl font-outfit font-semibold ">
-          Review Vocabulary: {{ currentCategory.name }}
+        <h1 class="text-title text-[86px] font-alkatra font-semibold">
+          Review Vocabulary
+          <!-- : {{ currentCategory.name }} -->
         </h1>
       </div>
 
-      <div class="flex flex-row gap-14 justify-center mt-3">
+      <div class="flex flex-row gap-16 justify-center mt-10">
+        <div
+          class="h-[400px] w-[300px] rounded-3xl shadow-md bg-white cursor-pointer hover:border-8 border-slate-200"
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <!-- Sound-Part -->
+          <SoundControl :soundPath="item.pronunciation">
+            <img
+              :src="item.src"
+              :alt="item.meaning"
+              class="w-[220px] h-[225px] ml-10 mt-3"
+            />
+          </SoundControl>
 
-        <div v-for="(item, index) in items" :key="index">
-
-          <div class=" flex flex-col justify-center h-[400px] w-[280px] rounded-3xl shadow-md bg-white ">
-
-            <div class="flex flex-col items-center ">
-              <img :src="item.src" :alt="item.word" class="w-[200px] h-[205px]">
-            </div>
-
-
-            <div class="border-solid border-b-2 border-black w-[230px] ml-6 my-3"></div>
-
-            <div class="flex justify-between">
-              <div class="flex flex-col font-outfit text-lg ml-10 mt-7">
-                <div> {{ item.word }} </div>
-                <div> {{ item.meaning }} </div>
-              </div>
-              <div class="mr-10 mt-12">เสียง</div>
-            </div>
-
-          </div>
-
+          <div
+            class="border-solid border-b-2 border-black w-[230px] ml-7 mt-10"
+          ></div>
+          <h1 class="text-[24px] ml-10 mt-3">
+            {{ item.word.split(' ')[0] }} <br />
+            {{ item.word.split(' ').slice(1).join(' ') }}
+          </h1>
+          <SoundButton />
         </div>
-
       </div>
 
-      <div class="flex flex-wrap justify-center mt-10">
+      <div class="flex flex-wrap justify-center mt-16">
         <div class="flex justify-center gap-6">
           <div
-            class="flex justify-center items-center  bg-title rounded-lg font-Outfit font-semibold text-lg p-4 text-white uppercase text-center hover:bg-button-bgColor sm:text-lg sm:p-3 sm:w-32">
-            <button> Play Again </button>
+            class="flex justify-center items-center bg-title rounded-lg font-Outfit font-semibold text-lg p-4 text-white text-center hover:bg-button-bgColor sm:text-lg sm:p-3 sm:w-40 h-16"
+          >
+            <button class="uppercase">Play Again</button>
           </div>
 
           <div
-            class="flex justify-center items-center bg-title rounded-lg font-Outfit font-semibold text-lg p-4 text-white uppercase text-center hover:bg-button-bgColor sm:text-lg sm:p-3 sm:w-32">
-            <button> Main Menu </button>
+            class="flex justify-center items-center bg-title rounded-lg font-Outfit font-semibold text-lg p-4 text-white text-center hover:bg-button-bgColor sm:text-lg sm:p-3 sm:w-40 h-16"
+          >
+            <button class="uppercase">Main Menu</button>
           </div>
         </div>
       </div>
-
     </div>
-
   </div>
-
 </template>
 
 <style scoped></style>
+
+<!-- 
+<div class="flex flex-row gap-16 justify-center mt-10">
+  <div
+    class="h-[400px] w-[300px] rounded-3xl shadow-md bg-white cursor-pointer hover:border-8 border-slate-200"
+    v-for="(item, itemIndex) in choices"
+    :key="itemIndex"
+  >
+
+    <SoundControl :soundPath="item.pronunciation">
+      <img
+        :src="item.src"
+        :alt="item.meaning"
+        class="w-[220px] h-[225px] ml-10 mt-3"
+      />
+    </SoundControl>
+
+    <div
+      class="border-solid border-b-2 border-black w-[230px] ml-7 mt-10"
+    ></div>
+    <h1 class="text-[24px] ml-10 mt-3">
+      {{ item.word.split(' ')[0] }} <br />
+      {{ item.word.split(' ').slice(1).join(' ') }}
+    </h1>
+    <SoundButton />
+  </div>
+</div> -->
---
+++ src/components/SoundButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <img
+    src="../../public/SoundButton.svg"
+    alt="SoundButton"
+    class="absolute ml-52 -mt-12"
+  />
+</template>
