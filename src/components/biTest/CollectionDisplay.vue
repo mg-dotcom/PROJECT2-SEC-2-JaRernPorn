7254d7fe20a83421b0b3c11d808f5d55a0822e69
ONTTHDO <111364693+ONTTHDO@users.noreply.github.com>
2024-03-19 22:58:36 +0700

    Update game2

---
+++ src/components/biTest/CollectionDisplay.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+const props=defineProps({
+    collectionName:String,
+})
+
+</script>
+
+<template>
+  <div>
+    <h2 class="text-lg font-semibold mb-2">Created Collection:</h2>
+    <p class="text-gray-800">{{ collectionName }}</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/flashcard/FlashCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+import AddNewCollection from './collection/AddNewCollection.vue';
+import Collection from './collection/Collection.vue';
+
+const showAddNewCollection = ref(false);
+const collectionName = ref('');
+const toggleAddNewCollection=()=>{
+  showAddNewCollection.value=!showAddNewCollection.value
+}
+
+const handleCollectionAdded = (name) => {
+  collectionName.value = name;
+};
+
+</script>
+
+<template>
+  <div>
+    <div class="header">
+      <h1 class="text-2xl font-bold ">Flashcard</h1>
+    </div>
+
+    <div class="addNew mt-4">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="toggleAddNewCollection">Add new +</button>
+      <hr class="my-4"/>
+    </div>
+
+    <div>
+        <Collection :collectionName="collectionName"/>
+    </div>
+
+    <div v-if="showAddNewCollection">
+        <AddNewCollection @collectionAdded="handleCollectionAdded"/>
+    </div>
+
+  </div>
+</template>
+
+<style scoped></style>
