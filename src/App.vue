6b7ccf75eaa41c9bde367e8aea30821235829a38
h88ych <hathaitat88@gmail.com>
2024-03-20 21:37:23 +0700

    Adding Game4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import Game1 from './components/Game1/Game1.vue'
 import Game2 from './components/game2/Game2.vue'
-import Game3 from './components/game3/game3.vue'
+// import Game3 from './components/game3/game3.vue'
+import Game4 from './components/Game4/Game4.vue'
 </script>
 <template>
   <div>
     <!-- <Game1></Game1> -->
     <!-- <Game2 /> -->
     <!-- <Game3 /> -->
+    <Game4 />
   </div>
 </template>
 <style scoped></style>
