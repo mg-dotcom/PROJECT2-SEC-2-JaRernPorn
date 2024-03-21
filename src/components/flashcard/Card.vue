c2430d7d1fc598c7c482266f983cc34eb4ebbb4c
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-03-22 00:22:19 +0700

    responsive Flashcard

--- src/components/flashcard/Card.vue
+++ src/components/flashcard/Card.vue
@@ -71,12 +71,14 @@ const showMeaning = () => {
       ></ranameFlashcard>
 
       <img
-        class="z-40 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] hover:bg-gray-200 rounded-full w-5 h-10 p-2 scale-[75%] absolute top-0 -right-[23px] transition-all duration-[270ms]"
+        class="z-40 bg-amber-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] hover:bg-amber-300 rounded-full w-5 h-10 p-2 scale-[75%] absolute xl:top-0 xl:-right-[19px] lg:top-3 lg:-right-[8px] md:top-0 md:-right-[19px] transition-all duration-[270ms]"
         src="/img/flashcard-pic/option.svg"
         alt="option"
         @click="showOption(props.index)"
       />
-      <div class="w-52 h-64 cursor-pointer group perspective bg-transparent">
+      <div
+        class="w-52 h-64 cursor-pointer group perspective bg-transparent xl:scale-100 lg:scale-90 md:scale-100 sm:scale-90 mobile:scale-90"
+      >
         <div
           class="relative preserve-3d group-hover:my-rotate-y-30 w-full h-full duration-1000 cursor-pointer"
           :class="{ 'my-rotate-y-150': isClicked }"
@@ -87,7 +89,7 @@ const showMeaning = () => {
           >
             <div class="flex flex-col h-full justify-center">
               <div
-                class="flex items-center justify-center font-medium break-all lg:text-5xl"
+                class="flex items-center justify-center font-medium break-all xl:text-5xl lg:text-5xl md:text-5xl sm:text-3xl"
                 :class="{
                   'h-auto':
                     props.computedFlashcards[props.index].chineseWord.length >
@@ -101,7 +103,7 @@ const showMeaning = () => {
               </div>
               <hr class="my-2 border-gray-300 dark:border-gray-700" />
               <div
-                class="flex items-center justify-center lg:text-2xl break-all"
+                class="flex items-center justify-center xl:text-2xl lg:text-2xl md:text-xl break-all"
                 :class="{
                   'h-auto':
                     props.computedFlashcards[props.index].pinyin.length > 15,
@@ -118,7 +120,9 @@ const showMeaning = () => {
             class="absolute my-rotate-y-200 backface-hidden bg-white border-[#FF9E94] py-4 px-7 rounded-xl border-2 w-full h-full overflow-y-auto"
           >
             <div class="flex flex-col h-full items-center justify-center">
-              <div class="font-medium break-all text-center lg:text-xl">
+              <div
+                class="font-medium break-all text-center xl:text-xl lg:text-xl md:text-2xl"
+              >
                 {{ props.computedFlashcards[props.index].meaning }}
               </div>
             </div>
