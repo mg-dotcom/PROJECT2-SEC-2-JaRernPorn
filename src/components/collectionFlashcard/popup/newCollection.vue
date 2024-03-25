d811700a50812bb283f7203c8ce48f50f3cf9703
ONTTHDO <111364693+ONTTHDO@users.noreply.github.com>
2024-03-26 02:19:36 +0700

    fix bugs adding and editing collection name

--- src/components/collectionFlashcard/popup/newCollection.vue
+++ src/components/collectionFlashcard/popup/newCollection.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 
 const closeButton = () => {
   props.popup.newCollection = false;
-  newCollectionName.value = "New Collection";
+  newCollectionName.value = "";
 };
 
 const addNewCollectionName = () => {
@@ -25,12 +25,23 @@ const addNewCollectionName = () => {
 
 const emit = defineEmits(["addNewCollections"]);
 
-const newCollectionName = ref("New Collection");
+const newCollectionName = ref("");
+const emptyCollectionName = ref(false)
 
-const passAndClear = (name, event) => {
-  emit("addNewCollections", name);
-  newCollectionName.value = "New Collection";
-  props.popup.newCollection = false;
+const passAndClear = (name) => {
+  console.log(name, name.length);
+  if (name.length === 0) {
+    emptyCollectionName.value = true
+    return
+  } else {
+    emit("addNewCollections", name);
+    newCollectionName.value = "";
+    props.popup.newCollection = false;
+  }
+};
+
+const isEmpty = (name) => {
+  return name === "";
 };
 </script>
 
@@ -38,10 +49,8 @@ const passAndClear = (name, event) => {
   <!-- Add New Collection -->
   <div class="text-start font-outfit text-base font-semibold pb-2">
     <div @click.self="props.closeOption">
-      <h1
-        class="cursor-pointer inline hover:bg-[#f4ead8] p-[4px] rounded-xl transition-all duration-[270ms]"
-        @click="addNewCollectionName"
-      >
+      <h1 class="cursor-pointer inline hover:bg-[#f4ead8] p-[4px] rounded-xl transition-all duration-[270ms]"
+        @click="addNewCollectionName">
         Add new +
       </h1>
     </div>
@@ -49,55 +58,47 @@ const passAndClear = (name, event) => {
   <hr class="border-gray-300" />
 
   <!-- Popup -->
-  <section
-    class="popup-newCollection z-50 fixed top-0 left-0"
-    v-show="props.popup.newCollection"
-  >
-    <div
-      class="bg-black bg-opacity-50 flex items-center justify-center min-h-screen w-screen"
-    >
+  <section class="popup-newCollection z-50 fixed top-0 left-0" v-show="props.popup.newCollection">
+    <div class="bg-black bg-opacity-50 flex items-center justify-center min-h-screen w-screen">
       <div
-        class="bg-white rounded-lg xl:scale-100 xl:w-[570px] xl:h-[350px] relative p-6 lg:w-[570px] lg:h-[350px] md:scale-[80%] sm:scale-[70%] mobile:scale-[73%] mobile:w-[500px]"
-      >
-        <closeIcon
-          class="absolute top-3 right-4 cursor-pointer"
-          @click="closeButton"
-        />
+        class="bg-white rounded-lg xl:scale-100 xl:w-[570px] xl:h-[350px] relative p-6 lg:w-[570px] lg:h-[350px] md:scale-[80%] sm:scale-[70%] mobile:scale-[73%] mobile:w-[500px]">
+        <closeIcon class="absolute top-3 right-4 cursor-pointer" @click="closeButton" />
         <div class="flex flex-col items-center justify-center">
           <div class="text-center text-3xl font-mono font-semibold">
             Add new collection
           </div>
           <div class="my-7 flex flex-col items-center justify-center">
-            <img
-              class="w-[178px]"
-              src="/img/flashcard-pic/collection.svg"
-              alt=""
-            />
-            <div
-              class="w-[160px] overflow-hidden absolute flex items-center justify-center"
-            >
-              <div
-                class="text-2xl font-outfit font-medium whitespace-normal break-all overflow-ellipsis max-h-[122px]"
-              >
+            <img class="w-[178px]" src="/img/flashcard-pic/collection.svg" alt="" />
+            <div class="w-[160px] overflow-hidden absolute flex items-center justify-center">
+              <div class="text-2xl font-outfit font-medium whitespace-normal break-all overflow-ellipsis max-h-[122px]">
                 {{ newCollectionName }}
               </div>
             </div>
           </div>
+
           <div class="flex gap-3">
-            <input
-              type="text"
-              v-model="newCollectionName"
+            <div class="flex flex-col items-end gap-1">
+              <input type="text" v-model.trim="newCollectionName"
               class="border-2 border-[#4096ff] rounded-md p-2 w-[400px] focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
+              :class="{
+                  'border-red-600': emptyCollectionName,
+                  'focus:border-red-600': emptyCollectionName,
+                  'border-[#4096ff]': !emptyCollectionName,
+                }"
               placeholder="Collection name"
-              @focus="$event.target.select()"
-              @keydown.enter="passAndClear(newCollectionName, $event)"
-            />
+              @keydown.enter="passAndClear(newCollectionName)"
+              @input="emptyCollectionName = isEmpty(newCollectionName)" />
+
+            <div v-if="emptyCollectionName" class="text-xs text-red-600 mr-1">
+              Please fill value in form
+            </div>
+            </div>
+
 
             <div>
               <button
                 class="bg-[#4096ff] text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
-                @click="passAndClear(newCollectionName, $event)"
-              >
+                @click="passAndClear(newCollectionName)">
                 ADD
               </button>
             </div>
--- src/components/collectionFlashcard/popup/renameCollection.vue
+++ src/components/collectionFlashcard/popup/renameCollection.vue
@@ -38,62 +38,63 @@ const closeButton = () => {
   props.popup.renameCollection = false;
 };
 // console.log(props.collectionId);
+
+const emptyCollectionName = ref(false)
 const toUpdateName = () => {
-  emit("changeCollectionName", props.index, renameCollectionName.value,props.collectionId);
+  if (renameCollectionName.value.length === 0) {
+    emptyCollectionName.value = true
+    return
+  } else {
+    emit("changeCollectionName", props.index, renameCollectionName.value, props.collectionId);
+  }
+};
+
+
+
+const isEmpty = (name) => {
+  return name === "";
 };
 </script>
 
 <template>
   <!-- Rename Collection Popup  -->
   <section class="popup-renameCollection z-50 fixed top-0 left-0">
-    <div
-      class="bg-black bg-opacity-50 flex items-center justify-center min-h-screen w-screen relative z-0"
-    >
+    <div class="bg-black bg-opacity-50 flex items-center justify-center min-h-screen w-screen relative z-0">
       <div
-        class="bg-white rounded-lg xl:scale-100 xl:w-[580px] xl:h-[350px] relative p-6 lg:w-[570px] lg:h-[350px] md:scale-[80%] sm:scale-[70%] mobile:scale-[58%] mobile:w-[500px]"
-      >
-        <closeIcon
-          class="absolute top-3 right-4 cursor-pointer"
-          @click="closeButton"
-        />
+        class="bg-white rounded-lg xl:scale-100 xl:w-[580px] xl:h-[350px] relative p-6 lg:w-[570px] lg:h-[350px] md:scale-[80%] sm:scale-[70%] mobile:scale-[58%] mobile:w-[500px]">
+        <closeIcon class="absolute top-3 right-4 cursor-pointer" @click="closeButton" />
         <div class="flex flex-col items-center justify-center">
           <div class="text-center text-3xl font-mono font-bold">
             Rename collection
           </div>
           <div class="my-7 flex flex-col items-center justify-center">
-            <img
-              class="w-[178px]"
-              src="/img/flashcard-pic/collection.svg"
-              alt="collection-pic"
-            />
-            <div
-              class="w-[178px] overflow-hidden absolute flex items-center justify-center"
-            >
+            <img class="w-[178px]" src="/img/flashcard-pic/collection.svg" alt="collection-pic" />
+            <div class="w-[178px] overflow-hidden absolute flex items-center justify-center">
               <div
-                class="text-2xl font-semibold font-outfit whitespace-normal break-all overflow-ellipsis max-h-[127px]"
-              >
-                {{
-                  renameCollectionName !== ""
-                    ? renameCollectionName
-                    : props.computedCollections[props.index].name
-                }}
+                class="text-2xl font-semibold font-outfit whitespace-normal break-all overflow-ellipsis max-h-[127px]">
+                {{ renameCollectionName !== ""  ? renameCollectionName : props.computedCollections[props.index].name }}
               </div>
             </div>
           </div>
           <div class="flex gap-3">
-            <input
-              type="text"
-              v-model="renameCollectionName"
-              class="border-2 border-[#4096ff] rounded-md p-2 w-[400px] focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
-              :placeholder="props.computedCollections[props.index].name"
-              @focus="$event.target.select()"
-              @keydown.enter="toUpdateName"
-            />
+            <div class="flex flex-col items-end gap-1">
+              <input type="text" v-model.trim="renameCollectionName"
+                class="border-2 border-[#4096ff] rounded-md p-2 w-[400px] focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
+                :class="{
+          'border-red-600': emptyCollectionName,
+          'focus:border-red-600': emptyCollectionName,
+          'border-[#4096ff]': !emptyCollectionName,
+        }" :placeholder="props.computedCollections[props.index].name" 
+                @keydown.enter="toUpdateName" @input="emptyCollectionName = isEmpty(renameCollectionName)" />
+
+              <div v-if="emptyCollectionName" class="text-xs text-red-600 mr-1">
+                Please fill value in form
+              </div>
+            </div>
             <div>
               <button
                 class="bg-[#4096ff] text-white rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#4096ff] focus:border-transparent transition-all duration-[270ms]"
-                @click="toUpdateName"
-              >
+                @click="toUpdateName">
                 OK
               </button>
             </div>
